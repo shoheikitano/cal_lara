74bf1dd51179de290f5035b2bf0bab2b5e97435a
shoheikitano <kitano.1105.shohei2598@gmail.com>
2019-12-17 19:53:08 +0900

    second commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','CalendarController@index');
+
+Route::get('holiday','CalendarController@getHoliday');
+Route::post('holiday','CalendarController@postHoliday');
+
+Route::get('holiday/{id}','CalendarController@getHolidayId');
+Route::delete('holiday','CalendarController@deleteHoliday');
